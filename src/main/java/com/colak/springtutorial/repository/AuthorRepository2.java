119c5d0b8411f03aae13ca50f8d98c70017a8a60
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-03 10:45:33 +0300

    findAll example

--- src/main/java/com/colak/springtutorial/repository/AuthorRepository2.java
+++ src/main/java/com/colak/springtutorial/repository/AuthorRepository2.java
@@ -4,15 +4,22 @@ import com.colak.springtutorial.jpa.Author;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuthorRepository2 extends JpaRepository<Author, Long> {
 
 
     // See https://medium.com/jpa-java-persistence-api-guide/springdata-n-1-solution-with-namedentitygraph-8b101292261a
-    // USe Spring EntityGraph
+    // Use Spring EntityGraph
+    // select a*,b.* from author a left join book b on a.id=b.author_id where a.id=?
     @EntityGraph(value = "author[books]")
     Optional<Author> findById(Long authorId);
 
+    // See https://medium.com/@aurigaaristo/8637a9a2399d
+    // Use Spring EntityGraph
+    // select * from author a left join book b on a.id=b.author_id
+    @EntityGraph(attributePaths = "books")
+    List<Author> findAll();
 
 }
